7fb1c9d681fb75cac274d86003979215e331c583
Tec7no <97606293+Tec7no@users.noreply.github.com>
2024-06-24 04:15:35 +0300

    update main

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import logging
-import smtplib
-import uuid
-from datetime import timedelta
-from contextvars import Token
 from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import pyotp
-
-from pydantic import BaseModel
-from starlette.middleware.cors import CORSMiddleware
-from fastapi import HTTPException
-from fastapi import BackgroundTasks
-from tortoise.transactions import in_transaction
-from datetime import datetime, timedelta
-from pydantic import BaseModel
 
 import jwt
 from fastapi import FastAPI, Request, HTTPException, status, Depends
@@ -33,9 +17,7 @@ from emails import *
 # response classes
 from fastapi.responses import HTMLResponse
 from models import user_pydanticIn, user_pydantic, business_pydantic, Business, Product, product_pydanticIn, \
-    product_pydantic, business_pydanticIn, PasswordResetToken, NormalUser, NormalUserLogin, NormalUserRegistration, \
-    MessageResponse, AuthToken, normal_user_pydanticIn, normal_user_pydantic, Account, account_pydantic, \
-    normal_user_pydanticOut
+    product_pydantic, business_pydanticIn
 
 # templates
 from fastapi.templating import Jinja2Templates
@@ -51,26 +33,95 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-
-oauth2_scheme_normal = OAuth2PasswordBearer(tokenUrl='token_normal')
+oath2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 # static file setup config
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+@app.post('/token')
+async def generate_token(request_from: OAuth2PasswordRequestForm = Depends()):
+    token = await token_generator(request_from.username, request_from.password)
+    return {"access_token" : token, "token_type" : "bearer"}
 
-async def get_current_normal_user(token_normal: str = Depends(oauth2_scheme_normal)):
+async def get_current_user(token: str = Depends(oath2_scheme)):
     try:
-        payload = jwt.decode(token_normal, config_credentials["SECRET"], algorithms=['HS256'])
-        user = await NormalUser.get(id=payload.get("id"))
-    except Exception:
+        payload = jwt.decode(token, config_credentials["SECRET"], algorithms=['HS256'])
+        user = await Vendor.get(id = payload.get("id"))
+
+    except:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid username or password",
             headers={"WWW-Authenticate": "Bearer"}
         )
-    return user
+    return await user
 
+
+@app.post("/user/me")
+async def user_login(user: user_pydanticIn = Depends(get_current_user)):
+    business = await Business.get(owner=user)
+    logo = business.logo #asl6as5d4.png
+    logo_path = "localhost:8000/static/images"+logo
+
+    return {
+        "status": "ok",
+        "data": {
+            "username": user.username,
+            "email": user.email,
+            "verified": user.is_verified,
+            "joined_date": user.join_date.strftime("%b %d %Y"),
+            "logo": logo_path
+        }
+    }
+
+
+@post_save(Vendor)
+async def create_business(
+        sender: "Type[Vendor]",
+        instance: Vendor,
+        created: bool,
+        using_db: "Optional[BaseDBAsyncClient]",
+        update_fields: List[str]
+) -> None:
+    if created:
+        business_obj = await Business.create(
+            business_name=instance.username, owner=instance
+        )
+
+        await business_pydantic.from_tortoise_orm(business_obj)
+        # send the email
+        await send_email([instance.email], instance)
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/verification', response_class=HTMLResponse)
+async def email_verification(request: Request, token: str):
+    try:
+        user = await verify_token(token)
+        if user and not user.is_verified:
+            user.is_verified = True
+            await user.save()
+            return templates.TemplateResponse("verification.html",
+                                              {"request": request, "username": user.username})
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+@app.post("/registration")
+async def user_registrations(user: user_pydanticIn):
+    user_info = user.dict(exclude_unset=True)
+    user_info["password"] = get_hashed_password(user_info["password"])
+    user_obj = await Vendor.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_obj)
+    return {
+        "status": "ok",
+        "data": f"Hello {new_user.username} , thanks for choosing snapmart. please check your email inbox and click on the link to verify your account .",
+    }
 
 
 @app.get("/")
@@ -79,7 +130,7 @@ def index():
 
 @app.post("/uploadfile/profile")
 async def create_upload_file(file: UploadFile = File(...),
-                             user: normal_user_pydantic = Depends(get_current_normal_user)):
+                             user: user_pydantic = Depends(get_current_user)):
     FILEPATH = "./static/images"
     filename = file.filename
     # test.png >> ["test", "png"]
@@ -103,12 +154,12 @@ async def create_upload_file(file: UploadFile = File(...),
 
     file.close()
 
-    account = await Account.get(owner = user)
-    owner = await account.owner
+    business = await Business.get(owner = user)
+    owner = await business.owner
 
     if owner == user:
-        account.logo = token_name
-        await account.save()
+        business.logo = token_name
+        await business.save()
     else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -119,132 +170,162 @@ async def create_upload_file(file: UploadFile = File(...),
     file_url = "localhost:8000"+ generate_name[1:]
     return {"status": "ok", "filename": file_url}
 
+@app.post("/uploadfile/product/{id}")
+async def create_upload_file(id: int, file: UploadFile = File(...),
+                             user: user_pydanticIn = Depends(get_current_user)):
+    FILEPATH = "./static/images"
+    filename = file.filename
+    # test.png >> ["test", "png"]
+    extenstion = filename.split(".")[1]
+
+    if extenstion not in ["png","jpg","jpeg"]:
+        return {"status" : "error", "detail": "File extenstion not allowed"}
+
+    # /static/images/asgasg.png
+    token_name = secrets.token_hex(10) + "."+extenstion
+    generate_name = FILEPATH + token_name
+    file_content = await file.read()
+
+    with open(generate_name, "wb") as file:
+        file.write(file_content)
+
+    # PILLOW
+    img = Image.open(generate_name)
+    img = img.resize(size = (200, 200))
+    img.save(generate_name)
+
+    file.close()
+
+    product = await Product.get(id=id)
+    business = await product.business
+    owner = await business.owner
+
+    if owner == user:
+        product.product_image = token_name
+        await product.save()
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated to perform this action",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    # Assuming your server is running locally on port 8000
+    file_url = "http://localhost:8000/static/images/" + token_name
+    return {"status": "ok", "filename": file_url}
+
+
+# CRUD functionality
+@app.post("/products")
+async def add_new_product(product: product_pydanticIn,
+                            user: user_pydantic = Depends(get_current_user)):
+
+    product = product.dict(exclude_unset=True)
+
+    # to avoid division error by zero
+    if product["original_price"] > 0:
+        product["percentage_discount"] = ((product["original_price"] - product["new_price"]) / product["original_price"]) * 100
+
+        product_obj = await Product.create(**product, business = user)
+        product_obj = await product_pydantic.from_tortoise_orm(product_obj)
+
+        return {"status": "ok", "data": product_obj}
+
+    else:
+        return {"status": "Error"}
 
 @app.post("/product")
 async def get_product():
     response = await product_pydantic.from_queryset(Product.all())
     return {"status":"ok", "data":response}
 
+@app.get("/product/{id}")
+async def get_product(id: int):
+    product = await Product.get(id = id)
+    business = await product.business
+    owner = await business.owner
+    response = await product_pydantic.from_queryset_single(Product.get(id = id))
 
+    return {
+        "status" : "ok",
+        "data": {
+            "product_details" : response,
+            "business_details": {
+                        "name" : business.business_name,
+                        "city" : business.city,
+                        "region": business.region,
+                        "description" : business.business_desription,
+                        "logo" : business.logo,
+                        "owner_id" : owner.id,
+                        "business_id": business.id,
+                        "join_date": owner.join_date.strftime("%b %d %Y")
+                    }
+                }
+            }
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000"
-]
+@app.delete("/product/{id}")
+async def delete_product(id: int , user: user_pydantic = Depends(get_current_user)):
+    product = await Product.get(id = id)
+    business = await product.business
+    owner = await business.owner
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-# إعداد تسجيل الأخطاء
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-db_url = "sqlite://database.sqlite3"
-
-register_tortoise(
-    app,
-    db_url=db_url,
-    modules={"models": ["models"]},
-    generate_schemas=True,
-    add_exception_handlers=True
-)
-
-
-
-# Token generator function (replace with your implementation)
-async def token_generator(username: str, password: str) -> str:
-    # Dummy implementation
-    user = await NormalUser.get(username=username)  # Assuming you validate the user here
-    payload = {"id": user.id, "exp": datetime.utcnow() + timedelta(hours=1)}
-    return jwt.encode(payload, config_credentials["SECRET"], algorithm='HS256')
-
-@app.post('/token_normal')
-async def generate_token(request_from: OAuth2PasswordRequestForm = Depends()):
-    token_normal = await token_generator(request_from.username, request_from.password)
-    return {"access_token": token_normal, "token_type": "bearer"}
-
-async def get_current_normal_user(token_normal: str = Depends(oauth2_scheme_normal)):
-    try:
-        payload = jwt.decode(token_normal, config_credentials["SECRET"], algorithms=['HS256'])
-        user = await NormalUser.get(id=payload.get("id"))
-    except Exception:
+    if user == owner:
+        product.delete()
+    else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid username or password",
+            detail="Not authenticated to perform this action",
             headers={"WWW-Authenticate": "Bearer"}
         )
-    return user
+    return {"status" : "ok"}
 
-@app.post("/normal_user/me")
-async def user_login(user: normal_user_pydanticIn = Depends(get_current_normal_user)):
-    account = await Account.get(owner=user)
-    logo = account.logo  # e.g., asl6as5d4.png
-    logo_path = f"http://localhost:8000/static/images/{logo}"
+@app.put("/product/{id}")
+async def update_product(id: int, update_info: product_pydanticIn, user: user_pydantic = Depends(get_current_user)):
+    product = await Product.get(id = id)
+    business = await product.business
+    owner = await business.owner
 
-    return {
-        "status": "ok",
-        "data": {
-            "username": user.username,
-            "email": user.email,
-            "verified": user.is_verified,
-            "joined_date": user.join_date.strftime("%b %d %Y"),
-            "logo": logo_path
-        }
-    }
+    update_info = update_info.dict(exclude_unset=True)
+    update_info["date_published"] = datetime.utcnow()
 
-@post_save(NormalUser)
-async def create_account(
-        sender: Type[NormalUser],
-        instance: NormalUser,
-        created: bool,
-        using_db: Optional[BaseDBAsyncClient],
-        update_fields: List[str]
-) -> None:
-    if created:
-        account_obj = await Account.create(account_name=instance.username, owner=instance)
-        await account_pydantic.from_tortoise_orm(account_obj)
-        await send_email([instance.email], instance)  # Assuming send_email is defined
-
-@app.get('/verification_normal', response_class=HTMLResponse)
-async def email_verification(request: Request, token_normal: str):
-    try:
-        user = await verify_token(token_normal)  # Assuming verify_token is defined
-        if user and not user.is_verified:
-            user.is_verified = True
-            await user.save()
-            return templates.TemplateResponse("verification.html", {"request": request, "username": user.username})
-        else:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="User is already verified or token is invalid",
-            )
-    except Exception:
+    if user == owner and update_info["original_price"] > 0:
+        update_info["percentage_discount"] = ((update_info["original_price"]
+                                               - update_info["new_price"]) / update_info["original_price"]) * 100
+        product = await product.update_from_dict(update_info)
+        product.save()
+        response = await product_pydantic.from_tortoise_orm(product)
+        return {"status": "ok", "date": response}
+    else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired token",
-            headers={"WWW-Authenticate": "Bearer"},
+            detail="Not authenticated to perform this action or invalid user input",
+            headers={"WWW-Authenticate": "Bearer"}
         )
 
-@app.post("/registration_normal")
-async def user_registrations(user: normal_user_pydanticIn):
-    user_info = user.dict(exclude_unset=True)
-    user_info["password"] = get_hashed_password(user_info["password"])  # Assuming get_hashed_password is defined
-    user_obj = await NormalUser.create(**user_info)
-    new_user = await normal_user_pydantic.from_tortoise_orm(user_obj)
-    return {
-        "status": "ok",
-        "data": f"Hello {new_user.username}, thanks for choosing snapmart. Please check your email inbox and click on the link to verify your account.",
-    }
+@app.put("/business/{id}")
+async def update_business(id: int, update_business: business_pydanticIn, user: user_pydantic = Depends(get_current_user)):
 
+    update_business = update_business.dict()
 
-@app.put("/normal-user/change-password")
-async def change_password_endpoint(new_password: str, current_password: str, user: normal_user_pydanticIn = Depends(get_current_normal_user)):
+    business = await Business.get(id=id)
+    business_owner = await business.owner
+
+    if user == business_owner:
+        await business.update_from_dict(update_business)
+        business.save()
+        response = await business_pydantic.from_tortoise_orm(business)
+        return {"status": "ok", "date": response}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated to perform this action",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+@app.put("/user/change-password")
+async def change_password_endpoint(new_password: str, current_password: str, user: user_pydanticIn = Depends(get_current_user)):
     # Retrieve user from database
-    user_obj = await NormalUser.get(username=user.username)
+    user_obj = await Vendor.get(username=user.username)
 
     # Verify the current password
     if not await verify_password(current_password, user_obj.password):
@@ -259,6 +340,14 @@ async def change_password_endpoint(new_password: str, current_password: str, use
 
     return {"status": "ok", "message": "Password updated successfully"}
 
+
+register_tortoise(
+    app,
+    db_url="sqlite://database.sqlite3",
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
 
 def main():
     import uvicorn
